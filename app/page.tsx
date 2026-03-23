ea26d5829a88d8f29a77a3d1d8def5ca78fa700f
Cybersport <Cybersport@26kadr.internal>
2026-03-23 16:30:37 +0300

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
 import Image from "next/image";
+import { getProducts } from "@/db/queries"
 
-export default function Home() {
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+export default async function CatalogPage({searchParams}: {searchParams: { page?: string }}) {
+  const sp = await searchParams
+  const page = Math.max(1, Number(sp.page) || 1)
+
+  // сколько товаров на странице
+  const pageSize = 9
+
+  // и товары и страницы
+  const { products, pages } = await getProducts(page, pageSize)
+
+  const visiblePages: number[] = []
+  for (let i = page - 2; i <= page + 2; i++) {
+    if (i > 0 && i <= pages) {
+      visiblePages.push(i)
+    }
+  }
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <main className="max-w-3xl mx-auto p-6 space-y-6">
+      <h1 className="text-xl font-semibold">Каталог</h1>
+
+      <div className="space-y-3">
+        {products.map((p) => (
+          <div key={p.id} className="border p-3 text-sm">
+            <div className="font-medium">{products.title}</div>
+            <div className="text-gray-500">{p.price} ₽</div>
+            <div>{p.description}</div>
+          </div>
+        ))}
+      </div>
+      {/* пагинация */}
+      <Pagination>
+        <PaginationContent>
+          {/* кнопка для предыдущей страницы */}
+          <PaginationItem>
+            <PaginationPrevious 
+              href={`?page=${Math.max(page - 1, 1)}`} 
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+          </PaginationItem>
+
+          {visiblePages.map((p) => (
+            <PaginationItem key={p}>
+              <PaginationLink 
+                href={`?page=${p}`} 
+                isActive={p === page}
+              >
+                {p}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          {/* кнопка для следующей страницы */}
+          <PaginationItem>
+            <PaginationNext 
+              href={`?page=${Math.min(page + 1, pages)}`} 
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </main>
+  )
 }
